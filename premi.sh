c99b1c10cef6c821ce30ca25df66ca4661237f7d
kuhing <134251508+kuhing@users.noreply.github.com>
2024-07-17 11:12:33 +0800

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -1120,4 +1120,6 @@ secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
 echo ""
 echo "===============-[ INSTALL SSH UDP & REBOOT ]-==============="
 sleep 3
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2" -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp
+wget -q https://raw.githubusercontent.com/sanakstore/sanakstore2/main/udp-custom.sh &&  chmod +x udp-custom.sh && ./udp-custom.sh
+sleep 2
+reboot
